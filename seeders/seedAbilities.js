4ba4d21fc7ae76519fef615a12a0cff813f115ef
CHIMERA <hex.penrose.io@gmail.com>
2025-09-16 05:54:48 -0400

    change to seeder code to call controllers

--- seeders/seedAbilities.js
+++ seeders/seedAbilities.js
@@ -3,9 +3,13 @@
 const { massLoader } = require("../services/bulkDataHandler");
 const { Ability } = require("../models");
 const abilitiesJSON = require("../datadumps/ability.json");
+const { abilityController } = require("../controllers/abilityController");
 
 const loadAbilities = async (transaction) => {
-  await massLoader(Ability, abilitiesJSON, transaction);
+  const abilityLoader = new abilityController(transaction);
+  await abilityLoader.createMultiple(abilitiesJSON);
+  console.log("loaded abilities");
+  // await massLoader(Ability, abilitiesJSON, transaction);
 };
 
 module.exports = loadAbilities;
--- seeders/seedSkills.js
+++ seeders/seedSkills.js
@@ -1,24 +1,28 @@
 "use strict";
+const { skillController } = require("../controllers/skillController");
 const skillJSON = require("../datadumps/skill.json");
 const { Skill, Ability } = require("../models");
 const { massLoader } = require("../services/bulkDataHandler");
 const { findEvery, findOne } = require("../services/queryHandler");
 const { AbilityMapper } = require("../utilities/helpers");
 const loadSkills = async (transaction) => {
-  const abilityMap = await AbilityMapper.getAbilityMap();
-  let skillsData = skillJSON.map((skill) => {
-    const loadAbility = abilityMap[skill.ability];
-    // const cond = {
-    //   where: { abilityName: skill.ability },
-    // };
-    // const dbAbility = await findOne(Ability, null, cond);
-    return {
-      skillName: skill.skillName,
-      ability: skill.ability,
-      abilityId: loadAbility,
-    };
-  });
-  await massLoader(Skill, skillsData, transaction);
+  const skillsLoader = new skillController(transaction);
+  await skillsLoader.createMultiple(skillJSON);
+  console.log("loaded skills");
+  // const abilityMap = await AbilityMapper.getAbilityMap();
+  // let skillsData = skillJSON.map((skill) => {
+  //   const loadAbility = abilityMap[skill.ability];
+  //   // const cond = {
+  //   //   where: { abilityName: skill.ability },
+  //   // };
+  //   // const dbAbility = await findOne(Ability, null, cond);
+  //   return {
+  //     skillName: skill.skillName,
+  //     ability: skill.ability,
+  //     abilityId: loadAbility,
+  //   };
+  // });
+  // await massLoader(Skill, skillsData, transaction);
 };
 
 module.exports = loadSkills;
