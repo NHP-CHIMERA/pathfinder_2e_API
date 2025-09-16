e52ecdd01713be8c5112d5c2d700a369415ea5b2
CHIMERA <hex.penrose.io@gmail.com>
2025-09-16 05:53:42 -0400

    moved instance creation services into dataHandler

--- services/dataHandler.js
+++ services/dataHandler.js
@@ -1,13 +1,77 @@
+const util = require("node:util");
+
+/**
+ * @async
+ * @function create
+ * @param {typeof import('sequelize').Model} model
+ * @param {Object} data - the data to be inserted into the database
+ * @param {Object} [options] - the options to pass into the sequelize create function
+ * @param {Object} [options.createTransaction] - tranasction to run operation under
+ * @param {Object} [options.isReturning = false] - indicates whether to return created instances, default false
+ * @returns {Promise<import('sequelize').Model>}
+ */
+const create = async (model, data, options) => {
+  const { isReturning = true, createTransaction } = options;
+  try {
+    return await model.create(data, {
+      transaction: createTransaction,
+      returning: isReturning,
+    });
+  } catch (error) {
+    console.error(
+      `Error creating instance of model: ${
+        model.name
+      } when using data: ${util.inspect(data, {
+        depth: null,
+        colors: true,
+      })}, Error:`
+    );
+  }
+};
+
+/**
+ * @async
+ * @function createBulk
+ * @param {typeof import('sequelize').Model} model - specific database model
+ * @param {Object} data - the data to be inserted into the database
+ * @param {Object} [options] - the options to be used in the creation
+ * @param {Object} [options.bulkTransaction] - tranasction to run operation under
+ * @param {Object} [options.isReturning = false] - indicates whether to return created instances, default false
+ * @returns {Promise<import('sequelize').Model[]>}
+ */
+const createBulk = async (model, data, options) => {
+  try {
+    const { isReturning = false, bulkTransaction } = options;
+
+    return await model.bulkCreate(data, {
+      transaction: bulkTransaction,
+      returning: isReturning,
+    });
+  } catch (err) {
+    console.error(
+      `Error creating multiple instances of model: ${
+        model.name
+      } when using data: ${util.inspect(data, {
+        depth: null,
+        colors: true,
+      })}:`,
+      err.message
+    );
+    throw err;
+  }
+};
+
 /**
  * @async
  * @function findCreate
  * @param {typeof import('sequelize').Model} model
  * @param {Object} conditions - the conditions of the selection to find possibly already existing model instance
- * @param {Object} inputInfo - the data to use to create a new instance of the model
+ * @param {Object} options - the data to use to create a new instance of the model
  * @returns {(Promise<import('sequelize').Model>|null)}
  */
-const findCreate = async (model, conditions, inputInfo, returnRecord) => {
+const findCreate = async (model, conditions, options) => {
   try {
+    const { inputInfo, returnRecord } = options;
     if (returnRecord) {
       const [record, created] = await model.findOrCreate({
         where: conditions,
@@ -37,55 +101,8 @@ const findCreate = async (model, conditions, inputInfo, returnRecord) => {
     );
   }
 };
-/**
- * @async
- * @function create
- * @param {typeof import('sequelize').Model} model
- * @param {Object} data - the data to be inserted into the database
- * @param {Object} inputInfo - the data to use to create a new instance of the model
- * @returns {(Promise<import('sequelize').Model>|null)}
- */
-const create = async (model, data) => {
-  try {
-    result = model.create(data);
-  } catch (error) {
-    console.error(
-      `Error finding or creating if does not exist instance of model: ${
-        model.name
-      } when using conditions: ${util.inspect(conditions, {
-        depth: null,
-        colors: true,
-      })} and inputdata: ${util.inspect(inputInfo, {
-        depth: null,
-        colors: true,
-      })}`
-    );
-  }
-};
-
-/**
- * @async
- * @function bulkCreate
- * @param {typeof import('sequelize').Model} model
- * @param {Object} data - the data to be inserted into the database
- * @param {Object} inputInfo - the data to use to create a new instance of the model
- * @returns {(Promise<import('sequelize').Model>|null)}
- */
-const createBulk = async (model, data) => {
-  try {
-    model.bulkCreate(data);
-  } catch (error) {
-    console.error(
-      `Error loading mass of massData: ${util.inspect(massData, {
-        depth: null,
-        colors: true,
-        showHidden: true,
-      })} for ${model.name}. Error: `,
-      error
-    );
-  }
-};
 module.exports = {
   findCreate,
   create,
+  createBulk,
 };
