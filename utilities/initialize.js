2b43fec14c62b67edf7c1935fd744dc66bf27930
CHIMERA <hex.penrose.io@gmail.com>
2025-09-23 08:20:37 -0400

    added database wipe on development initialize restart

--- utilities/initialize.js
+++ utilities/initialize.js
@@ -7,9 +7,13 @@ const loadHeritages = require("../seeders/seedHeritages");
 
 const initializeDb = async () => {
   try {
+    await db.sequelize.query(`DROP SCHEMA IF EXISTS public CASCADE;`);
+    await db.sequelize.query(`CREATE SCHEMA IF NOT EXISTS public;`);
+    await db.sequelize.query(`GRANT ALL ON SCHEMA public TO postgres;`);
+    await db.sequelize.query(`GRANT ALL ON SCHEMA public TO public;`);
     await db.sequelize.sync({ force: true });
     await db.sequelize.transaction(async (t) => {
-      await loadAbilities(t);
+      // await loadAbilities(t);
       await loadSkills(t);
       // await loadTraits(t);
       // await loadAncestries(t);
